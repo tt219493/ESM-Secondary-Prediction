39ff9a9969c4010717dab42c738adf8e000469db
tt219493 <tt219493@gmail.com>
2026-03-17 17:24:12 -0700

    Additional modules

--- module.py
+++ module.py
@@ -2,7 +2,8 @@ from transformers import EsmForTokenClassification
 import lightning as L
 import torch
 from torchmetrics.functional import accuracy
-from torch import optim
+from torch import optim, nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import polars as pl
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -308,3 +309,429 @@ class EsmForSecondaryStructure(L.LightningModule):
       scheduler = decay_scheduler
 
       return [optimizer], [scheduler]
+
+class NsForSecondaryStructure(L.LightningModule):
+    def __init__(self,
+                 num_labels = 10,
+                 combine_label = False,
+                 esm_eval = True,
+                 esm_pretrained = "facebook/esm2_t6_8M_UR50D",
+                 esm_ckpt = None,
+                ):
+        super().__init__()
+        self.num_labels = num_labels
+        self.combine_label = combine_label
+
+        self.esm = EsmForSecondaryStructure(num_labels=num_labels,
+                                            pretrained=esm_pretrained,
+                                            ckpt_path=esm_ckpt,
+                                            combine_label=combine_label)
+        
+        if esm_eval:
+          self.esm.eval()
+        else:
+          self.esm.train()
+
+        in_channels = self.esm.model.config.hidden_size
+        self.conv = nn.ModuleList([
+                        nn.Conv1d(in_channels, out_channels=32, kernel_size=129, padding=64),
+                        nn.Conv1d(in_channels, out_channels=32, kernel_size=257, padding=128)
+                    ])
+        self.batchnorm = nn.BatchNorm1d(in_channels + (2 * 32))
+        self.lstm = nn.LSTM(input_size = in_channels + (2 * 32),
+                            hidden_size = 1024,
+                            num_layers = 2,
+                            batch_first = True,
+                            dropout = 0.5,
+                            bidirectional = True
+                           )
+        self.dropout = nn.Dropout(0.5)
+        self.linear = nn.Linear(2 * 1024, num_labels)
+        self.accuracy = accuracy
+        
+    def compute_accuracy(self, predictions, labels):
+        if torch.where((labels == -100), 0, labels).sum() == 0:
+          acc = None
+        else:
+          acc = self.accuracy(
+              predictions,
+              labels,
+              num_classes=self.num_labels,
+              task="multiclass", 
+              ignore_index = -100
+          )
+        return acc
+
+    def forward(self, batch):
+        embedding = self.esm.get_embedding(batch)
+        # (N, L, C) -> (N, C, L)
+        x = embedding.permute(0, -1, 1)
+        max_len = x.shape[-1]
+        lens = torch.where(batch['label'] != -100, 1, 0).sum(axis=1).int().to(torch.device('cpu'))
+
+        tmp = x
+        for conv_layer in self.conv:
+            tmp = torch.cat([tmp, conv_layer(x)], dim=1)
+
+        x = self.batchnorm(tmp)
+
+        # (N, C, L) -> (N, L, C)
+        x = x.permute(0, -1, 1)
+        x = pack_padded_sequence(x, lens, batch_first=True, enforce_sorted=False)
+        x, _ = self.lstm(x)
+        x, _ = pad_packed_sequence(x, total_length=max_len, batch_first=True)
+        x = self.dropout(x)
+
+        
+        x = self.linear(x)
+
+        # (N, L, C) -> (N, C, L)
+        return x.permute(0, -1, 1)
+
+    def training_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+            
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("train_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+          return loss
+        else:
+          return torch.tensor(0.0).requires_grad_()
+
+    def validation_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+        
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("val_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("test_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+    def get_embedding(self, batch):
+        with torch.no_grad():
+            embedding = self.esm.get_embedding(batch)
+            # (N, L, C) -> (N, C, L)
+            x = embedding.permute(0, -1, 1)
+            max_len = x.shape[-1]
+            lens = torch.where(batch['label'] != -100, 1, 0).sum(axis=1).int().to(torch.device('cpu'))
+    
+            tmp = x
+            for conv_layer in self.conv:
+                tmp = torch.cat([tmp, conv_layer(x)], dim=1)
+    
+            x = self.batchnorm(tmp)
+    
+            # (N, C, L) -> (N, L, C)
+            x = x.permute(0, -1, 1)
+            x = pack_padded_sequence(x, lens, batch_first=True, enforce_sorted=False)
+            x, _ = self.lstm(x)
+            embedding, _ = pad_packed_sequence(x, total_length=max_len, batch_first=True)
+            return embedding
+
+    def predict_step(self, batch, batch_idx):
+        embedding = self.get_embedding(batch)   
+        return pl.LazyFrame([{'input_ids'      : batch['input_ids'].tolist(),
+                              'label'          : batch['label'].tolist(),
+                              'embedding'      : embedding.tolist()}])
+
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=5e-4)
+        return optimizer
+    
+class EncoderForSecondaryStructure(L.LightningModule):
+    def __init__(self,
+                 out_features = 320,
+                 num_labels = 10,
+                 combine_label = False,
+                 esm_eval = True,
+                 esm_pretrained = "facebook/esm2_t6_8M_UR50D",
+                 esm_ckpt = None,
+                ):
+        super().__init__()
+        self.num_labels = num_labels
+        self.combine_label = combine_label
+        self.esm = EsmForSecondaryStructure(num_labels=num_labels,
+                                            pretrained=esm_pretrained,
+                                            ckpt_path=esm_ckpt,
+                                            combine_label=combine_label)
+
+        self.hidden_size = self.out_features = out_features
+        
+        if esm_eval:
+          self.esm.eval()
+        else:
+          self.esm.train()
+
+        in_features = self.esm.model.config.hidden_size
+        self.relu = nn.ReLU()
+        self.encoder = nn.Linear(in_features, out_features)
+        self.classifier = nn.Linear(out_features, num_labels)
+
+        self.accuracy = accuracy
+        
+    def compute_accuracy(self, predictions, labels):
+        if torch.where((labels == -100), 0, labels).sum() == 0:
+          acc = None
+        else:
+          acc = self.accuracy(
+              predictions,
+              labels,
+              num_classes=self.num_labels,
+              task="multiclass", 
+              ignore_index = -100
+          )
+        return acc
+
+    def forward(self, batch):
+        # (N, L, C) 
+        x = self.esm.get_embedding(batch)
+        
+        x = self.encoder(x)
+        x = self.relu(x)
+        x = self.classifier(x)
+
+        # (N, C, L)
+        return x.permute(0, -1, 1)
+
+    def training_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+            
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("train_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+          return loss
+        else:
+          return torch.tensor(0.0).requires_grad_()
+
+    def validation_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+        
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("val_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("test_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+    def get_embedding(self, batch):
+        with torch.no_grad():
+            embedding = self.esm.get_embedding(batch)
+            embedding = self.encoder(embedding)
+        return embedding
+
+
+    def predict_step(self, batch, batch_idx):
+        embedding = self.get_embedding(batch)
+        return pl.LazyFrame([{'input_ids'      : batch['input_ids'].tolist(),
+                              'label'          : batch['label'].tolist(),
+                              'embedding'      : embedding.tolist()}])
+
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters())
+        return optimizer
+
+
+class NsEncoderForSecondaryStructure(L.LightningModule):
+    def __init__(self,
+                 out_features = 320,
+                 num_labels = 10,
+                 combine_label = False,
+                 esm_eval = True,
+                 esm_pretrained = "facebook/esm2_t6_8M_UR50D",
+                 esm_ckpt = None,
+                 ns_eval = True,
+                 ns_ckpt = None
+                ):
+        super().__init__()
+        self.num_labels = num_labels
+        self.combine_label = combine_label
+        self.ns = NsForSecondaryStructure(num_labels=num_labels,
+                                        combine_label=combine_label,
+                                        esm_eval=esm_eval,
+                                        esm_pretrained=esm_pretrained,
+                                        esm_ckpt=esm_ckpt
+                                       )
+        if ns_ckpt is not None:
+            self.ns.load_state_dict(torch.load(ns_ckpt, map_location=device)['state_dict'])
+
+
+        self.hidden_size = self.out_features = out_features
+        
+        if ns_eval:
+          self.ns.eval()
+        else:
+          self.ns.train()
+
+        in_features = 2048
+        self.relu = nn.ReLU()
+        self.encoder = nn.Linear(in_features, out_features)
+        self.classifier = nn.Linear(out_features, num_labels)
+
+        self.accuracy = accuracy
+        
+    def compute_accuracy(self, predictions, labels):
+        if torch.where((labels == -100), 0, labels).sum() == 0:
+          acc = None
+        else:
+          acc = self.accuracy(
+              predictions,
+              labels,
+              num_classes=self.num_labels,
+              task="multiclass", 
+              ignore_index = -100
+          )
+        return acc
+
+    def forward(self, batch):
+        # (N, L, C) 
+        x = self.ns.get_embedding(batch)
+        
+        x = self.encoder(x)
+        x = self.relu(x)
+        x = self.classifier(x)
+
+        # (N, C, L)
+        return x.permute(0, -1, 1)
+
+    def training_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+            
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("train_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+          return loss
+        else:
+          return torch.tensor(0.0).requires_grad_()
+
+    def validation_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+        
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("val_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch['label']
+        
+        loss = F.cross_entropy(y_hat, y)
+        predictions = y_hat.argmax(1)
+
+        if self.combine_label:
+           predictions = torch.where(predictions == 1, 0, predictions)
+           y = torch.where(y == 1, 0, y)
+
+        acc = self.compute_accuracy(predictions, y)
+
+        if acc:
+          self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+          self.log("test_acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+    def get_embedding(self, batch):
+        with torch.no_grad():
+            embedding = self.ns.get_embedding(batch)
+            embedding = self.encoder(embedding)
+        return embedding
+
+
+    def predict_step(self, batch, batch_idx):
+        embedding = self.get_embedding(batch)
+        return pl.LazyFrame([{'input_ids'      : batch['input_ids'].tolist(),
+                              'label'          : batch['label'].tolist(),
+                              'embedding'      : embedding.tolist()}])
+
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters())
+        return optimizer
